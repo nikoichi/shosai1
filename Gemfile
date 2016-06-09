562a59c28af1a789e8ba609395cbd85a9966a39d
Koichi Watanabe <monopori151@gmail.com>
2016-06-09 20:56:08 +0900

    fixed controller and view file

--- Gemfile
+++ Gemfile
@@ -46,3 +46,4 @@ group :development do
 end
 
 gem 'mechanize'
+gem 'pry-rails'
--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,9 @@
 class BooksController < ApplicationController
 
   def index
-
+    @recommendation_books_new = Book.limit(20).shuffle[0..4]
+    # binding.pry
+    @recommendation_books_all = Book.limit(20).shuffle[0..4]
+    @recommendation_books_generation = Book.limit(20).shuffle[0..4]
   end
 end
