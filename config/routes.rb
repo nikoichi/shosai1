b1f0bb045b8c2494ca4aad8fe889745b61ddf704
Koichi Watanabe <monopori151@gmail.com>
2016-06-13 14:35:42 +0900

    create reviews controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'books#index'
   resources :books, only: :show do
+    resources :reviews, only: [:new, :create]
     collection do
       get 'search'
     end
