ffd4f509b17fce5dda9aee3af9c723ac34b19607
Koichi Watanabe <monopori151@gmail.com>
2016-06-18 18:26:42 +0900

    searchファイルの修正、リンクの修正

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -11,7 +11,7 @@ class ReviewsController < ApplicationController
 
   private
   def create_params
-    params.require(:review).permit(:verall_rate, :change_life_rate, :learning_rate, :sympathy_rate, :interesting_rate, :impression_rate, :under_elementary_school_age_rate, :junior_high_school_student_rate, :igh_school_student_rate, :university_student_rate, :younger_20s_rate, :in_30s40s_rate, :in_50s60s_rate, :after_retirement_rate, :review_title, :review,)
+    params.require(:review).permit(:overall_rate, :change_life_rate, :learning_rate, :sympathy_rate, :interesting_rate, :impression_rate, :under_elementary_school_age_rate, :junior_high_school_student_rate, :high_school_student_rate, :university_student_rate, :younger_20s_rate, :in_30s40s_rate, :in_50s60s_rate, :after_retirement_rate, :review_title, :review,)
     .merge(book_id:params[:book_id])
   end
 end
