216a3e2af538546a449667fd11e150aec5897485
Koichi Watanabe <monopori151@gmail.com>
2016-06-22 15:04:17 +0900

    編集時は「レビュー一覧へ」ボタンを表示するように修正

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,14 +1,13 @@
 class ReviewsController < ApplicationController
   before_action :authenticate_user!, only: :new
+
   def new
     @book = Book.includes(:reviews).find(params[:book_id])
-    # @review = Review.where(book_id: params[:book_id], user_id: current_user.id).first_or_initialize
     @review = Review.new
-    # @myreview = []  #「レビューを書く」選択時に正しく条件分岐に入るために定義。
   end
 
   def create
-    Review.create(create_params)
+    Review.create(review_params)
     redirect_to book_path(params[:book_id]) # 書籍ページ(show)にリダイレクトする
   end
 
@@ -19,13 +18,15 @@ class ReviewsController < ApplicationController
    end
 
    def update
-     Review.update(create_params)
-     redirect_to book_path(params[:book_id]) # 書籍ページ(show)にリダイレクトする
+    review = Review.find(params[:id])
+    review.update(review_params)
+    redirect_to book_path(params[:book_id]) # 書籍ページ(show)にリダイレクトする
    end
 
   private
-  def create_params
-    params.require(:review).permit(:overall_rate, :change_life_rate, :learning_rate, :sympathy_rate, :interesting_rate, :impression_rate, :under_elementary_school_age_rate, :junior_high_school_student_rate, :high_school_student_rate, :university_student_rate, :younger_20s_rate, :in_30s40s_rate, :in_50s60s_rate, :after_retirement_rate, :review_title, :review,)
-    .merge(book_id: params[:book_id], user_id: current_user.id)
-  end
+    def review_params
+      params.require(:review).permit(:overall_rate, :change_life_rate, :learning_rate, :sympathy_rate, :interesting_rate, :impression_rate, :under_elementary_school_age_rate, :junior_high_school_student_rate, :high_school_student_rate, :university_student_rate, :younger_20s_rate, :in_30s40s_rate, :in_50s60s_rate, :after_retirement_rate, :review_title, :review,)
+      .merge(book_id: params[:book_id], user_id: current_user.id)
+    end
+
 end
